6c4c8369b555aeb2a401c2f0ed28a06189bb65ae
josecambareri <80832603+josecambareri@users.noreply.github.com>
2021-12-08 10:59:42 +0100

    Add files via upload
    push manuel

---
+++ src/application/action/ActionListeDesComptes.java
@@ -0,0 +1,28 @@
+package application.action;
+
+import banque.AgenceBancaire;
+
+public class ActionListeDesComptes implements Action{
+	
+	String message;
+	String code;
+	
+	@Override
+	public String actionMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String actionCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void execute(AgenceBancaire ag) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
